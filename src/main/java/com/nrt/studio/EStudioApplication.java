c3622e9fbf712cb0ac5d0017fcbb9553814e5950
Ramu Singh Thakur <ramusingh72103@gmail.com>
2024-04-25 17:45:47 +0530

    added the datasource configuration

--- src/main/java/com/nrt/studio/EStudioApplication.java
+++ src/main/java/com/nrt/studio/EStudioApplication.java
@@ -3,11 +3,16 @@ package com.nrt.studio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.log4j.Log4j2;
+
 @SpringBootApplication
+@Log4j2
 public class EStudioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EStudioApplication.class, args);
+		
+	 log.info("applicaiton is started successully ....");
 	}
 
 }
